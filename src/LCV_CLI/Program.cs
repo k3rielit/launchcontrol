881a981564a4fac03c879ec4dd920f1dce3292fb
szialajoscosplay <70654182+k3rielit@users.noreply.github.com>
2023-01-19 04:38:38 +0100

    deleted unnecessary constants

--- src/LCV_CLI/Program.cs
+++ src/LCV_CLI/Program.cs
@@ -9,9 +9,6 @@ using System.Diagnostics;
 
 namespace LCV_CLI {
     internal partial class Program {
-        const string mmpath = @"D:\Cheats\modest-menu_v0.9.7\";
-        const string gtapath = @"C:\Program Files\Epic Games\GTAV";
-
         static void Main(string[] args) {
             Console.OutputEncoding = Encoding.Unicode;
             Console.Title = "LaunchControlV Dev";
